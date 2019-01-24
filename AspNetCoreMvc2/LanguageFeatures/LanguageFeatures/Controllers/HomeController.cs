d708b3aa71941f7e0e57cc8a8df14ab2e4d1223a
Neil Thompson <neil@white-label-development.co.uk>
2019-01-24 13:43:25 +0000

    add an M, V and C

---
+++ AspNetCoreMvc2/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] {"C#", "Language", "Features"});
+        }
+    }
+}
