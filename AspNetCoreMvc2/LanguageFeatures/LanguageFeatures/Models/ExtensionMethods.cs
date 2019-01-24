793ede66f12695f8fe8549b1efe0fd3a920095bc
Neil Thompson <neil@white-label-development.co.uk>
2019-01-24 14:51:59 +0000

    ExtensionMethods

---
+++ AspNetCoreMvc2/LanguageFeatures/LanguageFeatures/Models/ExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public static class ExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (Product prod in cartParam.Products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+
+            //eg:
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            //decimal cartTotal = cart.TotalPrices();
+        }
+
+
+        //Extension applied to IEnumerable interface
+        //so it works on the ShppingCart (which implements this interface)
+        //but also works on others things, 
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+            foreach (Product prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+
+            //also works on an array 
+            //Product[] productArray = { new Product { Name = "Kayak", Price = 275M }, new Product { Name = "Lifejacket", Price = 48.95M } };
+            //decimal arrayTotal = productArray.TotalPrices();
+        }
+    }
+}
---
+++ AspNetCoreMvc2/LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+
+        //inherit from IEnumerable<Product> and implement these two methods for the Interface Extension Method example.
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
