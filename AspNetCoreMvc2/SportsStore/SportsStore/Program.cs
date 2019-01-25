7960f8dc67e8cbe819d7215df480d3dd790a5224
Neil Thompson <neil@white-label-development.co.uk>
2019-01-25 15:19:21 +0000

    add appsettings.json, inject configuration into Startup. ConfigureServices

--- AspNetCoreMvc2/SportsStore/SportsStore/Program.cs
+++ AspNetCoreMvc2/SportsStore/SportsStore/Program.cs
@@ -19,6 +19,7 @@ namespace SportsStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options => options.ValidateScopes = false); //not sure about ValidateScopes. Research...
     }
 }
--- AspNetCoreMvc2/SportsStore/SportsStore/Startup.cs
+++ AspNetCoreMvc2/SportsStore/SportsStore/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 
@@ -12,14 +14,21 @@ namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; } //receives the configuration data loaded from the appsettings.json file, which is presented through an object that implements the IConfiguration interface. 
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             // when a component, such as a controller, needs an implementation of the IProductRepository interface, it should receive an instance of the FakeProductRepository class. T
-            // he AddTransient method specifies that a new FakeProductRepository object should be created each time the IProductRepository interface is needed
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            // The AddTransient method specifies that a new FakeProductRepository object should be created each time the IProductRepository interface is needed
+            //services.AddTransient<IProductRepository, FakeProductRepository>(); //drop the fake, we are using EF now..
+            services.AddTransient<IProductRepository, EFProductRepository>();
 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:SportStoreProducts"]));
+                            
             services.AddMvc();
         }
 
