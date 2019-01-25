96018e5a55cb1777d9093e86f9b2d8de3f9333fc
Neil Thompson <neil@white-label-development.co.uk>
2019-01-25 09:52:56 +0000

    initial MVC setup

---
+++ AspNetCoreMvc2/SportsStore/SportsStore/Controllers/NeilController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class NeilController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AspNetCoreMvc2/SportsStore/SportsStore/Startup.cs
+++ AspNetCoreMvc2/SportsStore/SportsStore/Startup.cs
@@ -15,9 +15,12 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // The Configure method is used to set up the features that receive and process HTTP requests.
+        // Each method that I call in the Configure method is an extension method that sets up an HTTP request processor
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,9 +28,15 @@ namespace SportsStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+
             });
         }
     }
