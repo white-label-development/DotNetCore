31f719f2bd666257e82911a5f270d115825f591e
Neil Thompson <neil@white-label-development.co.uk>
2019-02-01 13:33:53 +0000

    explore IActionResult (as opposed to a concrete ViewResult or RedirectResult etc)

--- AspNetCoreMvc2/SportsStore/SportsStore/Controllers/NeilController.cs
+++ AspNetCoreMvc2/SportsStore/SportsStore/Controllers/NeilController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
 using SportsStore.Infrastructure;
 using SportsStore.Models;
@@ -14,33 +17,52 @@ namespace SportsStore.Controllers
         private readonly Neil _neilModel;
         private ILogger<NeilController> logger;
 
-        public NeilController(Neil neilModel, ILogger<NeilController> lo)
+        public NeilController(Neil neilModel, ILogger<NeilController> log)
         {
             _neilModel = neilModel;
-            logger.LogDebug($"Handled {Request.Path} at uptime {_neilModel.Uptime}");
+            logger = log;
+            //logger.LogDebug("logger ok");            
             //logger.LogCritical("ouch");
         }
 
         public IActionResult Index()
         {
+            logger.LogDebug($"Handled {Request.Path} at uptime {_neilModel.Uptime}");
             return View();
         }
 
-        public static int DescendingOrder(int num)
+        public string Uptime() => _neilModel.Uptime;
+
+        //manual construction of ViewResult
+        public ViewResult PocoAction() => new ViewResult()
         {
-            if (num < 0) throw new ArgumentException("num must be non-negative");
+            ViewName = "Result",
+            ViewData =
+                new ViewDataDictionary(
+                    new EmptyModelMetadataProvider(),
+                    new ModelStateDictionary())
+                {
+                    Model = new Result
+                    {
+                        Action = "a",
+                        Controller = "b"
+                    }
+                }
+        };
 
-            // Bust a move right here
-            var numbers = num.ToString().Select(c => (int)Char.GetNumericValue(c));
-
-
-            string numStr = numbers.OrderByDescending(x => x).Aggregate("", (current, i) => current + i);
-
-
-            return int.Parse(numStr);
+        //manual response via  HttpResponse context object
+        public void ReceiveForm(string name, string city)
+        {
+            Response.StatusCode = 200;
+            Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII.GetBytes($"<html><body>{name} lives in {city}</body>");
+            Response.Body.WriteAsync(content, 0, content.Length);
         }
 
 
-        public string Uptime() => _neilModel.Uptime;
+        //return a custom IActionResult (as opposed to a conrete ViewResult or RedirectResult etc)
+        public IActionResult ReceiveFormAndReturnCustomHtmlResult(string name, string city) => new CustomHtmlResult { Content = $"{name} lives in {city}" };
+
+
     }
 }
---
+++ AspNetCoreMvc2/SportsStore/SportsStore/Infrastructure/CustomHtmlResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Infrastructure
+{
+    public class CustomHtmlResult : IActionResult
+    {
+
+        public string Content { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 200;
+            context.HttpContext.Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII.GetBytes(Content);
+            return context.HttpContext.Response.Body.WriteAsync(content,0, content.Length);
+        }
+    }
+}
